8a508cc3a2f729a56d541f29377352b5162c9007
21vek <git@github.com>
2017-09-27 12:18:36 +0300

    1

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer>
     <section>
-        <div class="copy"><div><span ondblclick="self.location=&#39;/indeego.php&#39;;">©</span> 1999 <?php echo $site_name; ?></div></div>
+        <div class="copy"><div><span ondblclick="self.location=&#39;/indeego.php&#39;;">©</span> 2017 <?php echo $site_name; ?></div></div>
         <div class="addr"><b><?php echo $adres; ?></b><br><?php echo $vremia_raboti; ?><div><a href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a></div></div>
         <div class="links"><a href="articles.php">Пoлезные статьи</a><br><a href="gotovie_keisy.php">Готовые примеры</a><br><a href="job_creators.php">Работа авторам</a></div>
     </section>
