ab1495e7de6261ad3abf350339c957160785c389
21vek <git@github.com>
2017-10-20 18:21:58 +0300

    1

--- order.php
+++ order.php
@@ -208,24 +208,7 @@ require("header.php");
           </td>
         </tr>
 
-    <tr>
-        <td class="field">
-            <?php
-            $a = rand(1,10);
-            $b = rand(1,100);
-            $c = $a+$b;
-            echo $a."+".$b."=";
-            ?>
-            <span style="color: red;">*</span>
-        </td>
-        <td>
-            <input style="width: 50%;" type="text" name="count_1">
-            <input style="width: 50%; display: none;" type="text" value=<?php echo $c?> name="count_2">
-            <input style="width: 50%; display: none;" type="text" value="Не определено" name="university">
-
-
-        </td>
-    </tr>
+    
 
 
 
